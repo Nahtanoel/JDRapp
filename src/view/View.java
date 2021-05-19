5ac137f965fa0e10a63cc0ae4ed50c20d16f604f
Nahtanoel <78899527+Nahtanoel@users.noreply.github.com>
2021-05-19 17:42:21 +0200

    Create View.java

---
+++ src/view/View.java
@@ -0,0 +1,26 @@
+package view;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+public class View extends JFrame {
+    private MenuView menu;
+    private MainView principal;
+
+    public View(){
+        initialiseComponents();
+        setTitle("Programme");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+    }
+    public void initialiseComponents(){
+        ArrayList<ModuleFrame> modules = new ArrayList<ModuleFrame>();
+        modules.add(new CompteurFrame());
+        principal = new MainView(modules);
+        menu = new MenuView();
+        add(principal);
+        //add(menu);
+        //menu.setVisible(false);
+        principal.setVisible(true);
+    }
+
+}
