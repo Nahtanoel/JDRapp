c702c5564144c779a1c3ca5a0491b99fa05da25b
Nahtanoel <78899527+Nahtanoel@users.noreply.github.com>
2021-05-26 16:06:33 +0200

    Update View.java

--- src/view/View.java
+++ src/view/View.java
@@ -1,27 +1,46 @@
 package view;
 import javax.swing.*;
+
+import controler.actionListener;
+
 import java.awt.*;
 import java.util.ArrayList;
 public class View extends JFrame {
     private MenuView menu;
     private MainView principal;
+    private actionListener al;
 
-    public View(){
+    public View(actionListener al){
+        this.al=al;
         initialiseComponents();
         setTitle("Programme");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         
     }
     public void initialiseComponents(){
+
         ArrayList<ModuleFrame> modules = new ArrayList<ModuleFrame>();
-        modules.add(new CompteurFrame());
-        principal = new MainView(modules);
+        modules.add(new CompteurFrame("PV",al));
+        principal = new MainView(modules,al);
         menu = new MenuView();
         add(principal);
-        pack();
         //add(menu);
         //menu.setVisible(false);
         principal.setVisible(true);
     }
 
+    public void changeResultatText(String text){
+        this.principal.setResultatText(text);
+    }
+    public void afficheMenu(){
+        this.principal.setVisible(false);
+
+        this.menu.setVisible(true);
+        this.repaint();
+    }
+    public void augementer(){
+    }
+    public void decrease(){
+
+    }
 }
