69d1c4b15d84134ba40dcec0371b5ef5d005772c
Nahtanoel <78899527+Nahtanoel@users.noreply.github.com>
2021-05-26 17:26:11 +0200

    Update ListeFrame.java

--- src/view/ListeFrame.java
+++ src/view/ListeFrame.java
@@ -1,6 +1,41 @@
 package view;
 import javax.swing.*;
+
+import model.liste;
+
 import java.awt.*;
-public class ListeFrame {
-    
+import java.util.HashMap;
+public class ListeFrame extends ModuleFrame {
+    private JLabel nom;
+    private JPanel frame;
+    private JPanel Liste;
+    private liste laListe;
+    private int nbElement;
+
+
+    public ListeFrame(String nom){
+        this.nom = new JLabel(nom);
+        this.frame = new JPanel();
+        this.laListe = new liste(nom);
+        this.nbElement = 0;
+        Liste = new JPanel();
+        Liste.setLayout(new GridLayout(laListe.getElements().size(),1));
+
+        
+        for (String e : laListe.getElements().keySet()) {
+            if(laListe.getElements().get(e)){
+                
+                Liste.add(new JCheckBox(e, null, true));
+            }else{
+                Liste.add(new JCheckBox(e));
+            }
+        }
+
+        frame.setLayout(new BorderLayout());
+        frame.add(this.nom,BorderLayout.NORTH);
+        frame.add(this.Liste,BorderLayout.CENTER);
+        this.add(frame);
+
+
+    }
 }
